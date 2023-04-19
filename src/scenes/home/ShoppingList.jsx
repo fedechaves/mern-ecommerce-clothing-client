026315cee1ed21d9ece793b1d3cedfba8d3425a4
fedechaves <federicochavesf@gmail.com>
2023-04-18 20:08:02 -0400

    feat: shopping List modules

--- src/scenes/home/ShoppingList.jsx
+++ src/scenes/home/ShoppingList.jsx
@@ -1,6 +1,11 @@
-
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Box, Typography, Tab, Tabs, usMediaQuery } from "@mui/icons-material";
+import Item from "../../state";
 
 const ShoppingList = () => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("all");
   return (
     <div>
       
