d981197405640908c50791b86c596b853cdbcb77
fedechaves <federicochavesf@gmail.com>
2023-04-20 12:18:28 -0400

    checkout done

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -9,7 +9,7 @@ import Shipping from "./Shipping";
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
-  "pk_test_51LgU7yConHioZHhlAcZdfDAnV9643a7N1CMpxlKtzI1AUWLsRyrord79GYzZQ6m8RzVnVQaHsgbvN1qSpiDegoPi006QkO0Mlc"
+  "pk_test_51Mz04zBsYUOX6FbkLnrkaEmzVXF2TqUJ4ayQOD9OQX8KPSFz35NZ6FKRi2XmpD6Ul12ovyDtbzylkUPticjiiaOK000Kf8nKNp"
 );
 
 const Checkout = () => {
@@ -47,7 +47,7 @@ const Checkout = () => {
       })),
     };
 
-    const response = await fetch("http://localhost:2000/api/orders", {
+    const response = await fetch("http://localhost:1337/api/orders", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(requestBody),
