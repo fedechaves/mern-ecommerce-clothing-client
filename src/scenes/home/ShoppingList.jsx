c21e71702dffd5684b48dfcf1e471176cc0d121f
fedechaves <federicochavesf@gmail.com>
2023-06-16 17:03:25 -0400

    fetch call on sl

--- src/scenes/home/ShoppingList.jsx
+++ src/scenes/home/ShoppingList.jsx
@@ -21,7 +21,7 @@ const ShoppingList = () => {
 
   async function getItems() {
     const items = await fetch(
-      "https://strapi-production-clothing.up.railway.app/api/items?populate=image",
+      "https://strapi-production-clothing.up.railway.app/api/items",
       { method: "GET" ,
       headers: { "Content-Type": "application/json" }},
     );
